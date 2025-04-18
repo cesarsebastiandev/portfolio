3a4519fa14dcb82c81752e72a4e614abf6591f18
cesarsebastiandev <cesarsebastian.dev@gmail.com>
2025-04-18 14:30:43 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Router, RouterOutlet, Event, NavigationEnd } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { ContentComponent } from "./components/content/content.component";
+import { FooterComponent } from "./components/footer/footer.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => window.HSStaticMethods.autoInit(), 100);
+      }
+    });
+  }
 }
